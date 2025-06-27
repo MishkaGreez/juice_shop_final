b2106c4cdbbf4bf6b30f5be395b00042dd32de2d
MishkaGreez <123957225+MishkaGreez@users.noreply.github.com>
2025-06-27 13:44:16 +0300

    f:23

--- commit.ps1
+++ commit.ps1
@@ -8,6 +8,7 @@ git add .
 
 #test secret
 npm run gitleaks:staged
+.\kics\bin\kics.exe scan -p "D:\repo-uni\juice_shop_final" --report-formats json -o ./kics-results
 
 # Коммит с переданным сообщением
 git commit -m "$CommitMessage"
