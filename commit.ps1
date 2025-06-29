3e01fcb5ef7d4c5ab185990cde260bbb68ab7d97
MishkaGreez <123957225+MishkaGreez@users.noreply.github.com>
2025-06-29 12:58:17 +0300

    t:1

--- commit.ps1
+++ commit.ps1
@@ -7,9 +7,9 @@ param (
 git add .
 
 #test seq. and secret
-.\scan-retire-html -OutputFile "vulns.html"
+#.\scan-retire-html -OutputFile "vulns.html"
 npm run gitleaks:staged
-.\kics\bin\kics.exe scan -p "D:\repo-uni\juice_shop_final" --report-formats html -o ./kics-results
+#.\kics\bin\kics.exe scan -p "D:\repo-uni\juice_shop_final" --report-formats html -o ./kics-results
 
 # Коммит с переданным сообщением
 git commit -m "$CommitMessage"
